331b09cc2f130b22b3b20324e0dad0e6086e152a
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2015-10-03 16:56:40 +0200

    add new background

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,7 @@
 (function () {
   $(document).ready(function () {
     var backgrounds = [];
-    for (var i = 1; i <= 14; i++) {
+    for (var i = 1; i <= 15; i++) {
       backgrounds.push('bg-' + i);
     }
     $('#hero').addClass(backgrounds[Math.floor(Math.random() * backgrounds.length)]);
